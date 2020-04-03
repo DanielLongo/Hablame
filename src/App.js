5fe44686fbfc74cb2b4783e8806f140d4b957c1a
DanielLongo <danieldominiclongo@gmail.com>
2020-04-03 13:31:13 -0700

    automated updates

--- src/App.js
+++ src/App.js
@@ -25,40 +25,88 @@ function initializeReactGA() {
     ReactGA.pageview('/');
 }
 initializeReactGA()
-function App() {
-      return (
-        <div className="App">
-            <BrowserView>
-                <Router>
-                    <Switch>
-                        <Route path="/conversation">
-                            <PraticeConversation />
-                        </Route>
-                        <Route path="/presentation">
-                            <PraticePresentation />
-                        </Route>
-                        <Route path="/accent">
-                            <PraticeAccent />
-                        </Route>
-                        <Route path="/teachers">
-                            <InformationForTeachers/>
-                        </Route>
-                        <Route path="/">
-                            <Home />
-                        </Route>
-                    </Switch>
-                </Router>
-            </BrowserView>
-            <MobileView>
-                <img src={"logo.png"} style={{width:200, marginTop:"1%"}}/>
-                <p style={{marginTop: "4%", fontSize:17, marginLeft:"2%", marginRight:"2%"}}
-                >Unfortunately, Hablame.org is not available on mobile devices. <strong>Please visit our website from a computer.</strong> For help please email support@hablame.org</p>
-                <p style={{marginTop: "10%", fontSize:23, marginLeft:"2%", marginRight:"2%", fontWeight:"bold"}}>What is Hablame?</p>
-                <p style={{marginTop: "0%", fontSize:17, marginLeft:"2%", marginRight:"2%"}}>Hablame.org is a free website dedicated to Spanish speaking practice. The site offers activities modeled after the AP Spanish exam including interpersonal and presentational speaking practice. <strong> The site also allows for students/learners to download their audio recordings so that they may save them or submit them to teachers. </strong></p>
-                <CustomFooter/>
-            </MobileView>
-        </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        console.log(window.location.href)
+    }
+
+    state = {
+        isFrench : this.isFrench(),
+        isSpanish : this.isSpanish()
+
+    };
+
+    isFrench () {
+        if (window.location.href.includes("parlefrancais")) {
+            return true
+        }
+        return false
+    }
+
+    isSpanish() {
+        if (window.location.href.includes("hablame")) {
+            return true
+        }
+        return false
+    }
+
+    componentDidMount() {
+        console.log("state", this.state)
+        // if (this.state.isFrench && this.state.isSpanish) {
+        //     console.log("both spanish and french")
+        //     this.setState({
+        //         isFrench : false
+        //     })
+        // }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <BrowserView>
+                    <Router>
+                        <Switch>
+                            <Route path="/conversation">
+                                <PraticeConversation/>
+                            </Route>
+                            <Route path="/presentation">
+                                <PraticePresentation/>
+                            </Route>
+                            <Route path="/accent">
+                                <PraticeAccent/>
+                            </Route>
+                            <Route path="/teachers">
+                                <InformationForTeachers/>
+                            </Route>
+                            <Route path="/">
+                                <Home/>
+                            </Route>
+                        </Switch>
+                    </Router>
+                </BrowserView>
+                <MobileView>
+                    <img src={"logo.png"} style={{width: 200, marginTop: "1%"}}/>
+                    <p style={{marginTop: "4%", fontSize: 17, marginLeft: "2%", marginRight: "2%"}}
+                    >Unfortunately, Hablame.org is not available on mobile devices. <strong>Please visit our website
+                        from a computer.</strong> For help please email support@hablame.org</p>
+                    <p style={{
+                        marginTop: "10%",
+                        fontSize: 23,
+                        marginLeft: "2%",
+                        marginRight: "2%",
+                        fontWeight: "bold"
+                    }}>What is Hablame?</p>
+                    <p style={{marginTop: "0%", fontSize: 17, marginLeft: "2%", marginRight: "2%"}}>Hablame.org is a
+                        free website dedicated to Spanish speaking practice. The site offers activities modeled after
+                        the AP Spanish exam including interpersonal and presentational speaking practice. <strong> The
+                            site also allows for students/learners to download their audio recordings so that they may
+                            save them or submit them to teachers. </strong></p>
+                    <CustomFooter/>
+                </MobileView>
+            </div>
+        );
+    }
 }
 
 export default App;
