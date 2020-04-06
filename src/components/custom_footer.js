a1de2926df5f536eb6981fd59c12d3b8da0f8f3d
DanielLongo <danieldominiclongo@gmail.com>
2020-04-05 17:57:16 -0700

    automated updates

--- src/components/custom_footer.js
+++ src/components/custom_footer.js
@@ -28,10 +28,10 @@ class CustomFooter extends Component {
                 <div style={style}>
                     <p
                         style={{marginTop:"-.5%"}}
-                    >Please contact support@hablame.org | <a href={"https://hablame.org/"}>Hablame - Spanish</a> & <a href={"https://parlefrancais.org/"}>ParleFrancais - French</a></p>
+                    > <a href={"https://hablame.org/"}>Hablame - Spanish</a> & <a href={"https://parlefrancais.org/"}>ParleFrancais - French</a> | <a href={"privacy.html"}>Privacy Policy</a></p>
                     <p style={{marginTop:"-.8%"}}
                     />
-                    <p>Created by Daniel Longo 2020</p>
+                    <p> Please contact support@hablame.org | Created by Daniel Longo 2020</p>
                 </div>
             </div>
         )
