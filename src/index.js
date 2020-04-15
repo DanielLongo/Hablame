468555c6f128310e78eae0a8859c726c261ca636
DanielLongo <danieldominiclongo@gmail.com>
2020-04-14 17:33:00 -0700

    automated updates

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({dsn: "https://1bf69e1db47b4e1fb77d3efa51a81c5a@o377947.ingest.sentry.io/5200695"});
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
